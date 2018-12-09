25a444e1816cf6988662fe42ffc98936fa39e69c
jeongseok.han <jeongseok.han@DESKTOP-QLDFQJ2>
2018-12-10 00:40:14 +0900

    init project

--- src/main/java/healing/spot/apigateway/ApigatewayApplication.java
+++ src/main/java/healing/spot/apigateway/ApigatewayApplication.java
@@ -3,7 +3,9 @@ package healing.spot.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @EnableCircuitBreaker
 @SpringBootApplication
 public class ApigatewayApplication {
